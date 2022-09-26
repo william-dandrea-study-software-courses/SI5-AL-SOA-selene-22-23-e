a6ac4dd1bdc207c5a386a8f7e63d79c1b657ab36
Arthur-Soens <71391727+Arthur-Soens@users.noreply.github.com>
2022-09-26 22:01:12 +0200

    life support et module life bro pour la vie

--- life-support-service/src/life-support-supervision/controllers/life-support-supervision.controller.ts
+++ life-support-service/src/life-support-supervision/controllers/life-support-supervision.controller.ts
@@ -9,10 +9,15 @@ import {firstValueFrom} from "rxjs";
 @ApiTags('life-support-supervision')
 @Controller('/life-support-supervision')
 export class LifeSupportSupervisionController {
+
+  url: string
+
   constructor(
     private readonly moduleLifeSupervisionService: LifeSupportSupervisionService,
     private readonly httpService: HttpService,
-  ) {}
+  ) {
+    this.url = 'http://'+process.env.MODULE_LIFE_SERVICE_URL_WITH_PORT;
+  }
 
   @ApiOkResponse({ type: Boolean })
   @Get('/oui')
@@ -23,8 +28,8 @@ export class LifeSupportSupervisionController {
   @ApiOkResponse({ type: Boolean })
   @Get('/test')
   async testService2Service(): Promise<any> {
-    const rep : AxiosResponse = await firstValueFrom(this.httpService.get("http://"+process.env.MODULE_LIFE_SERVICE_URL_WITH_PORT + '/module-life-supervision/test'));
-    console.log(rep)
+    const rep : AxiosResponse<any> = await firstValueFrom(this.httpService.get(this.url+ '/module-life-supervision/test'));
+    console.log(rep);
     return rep.data;
   }
 }
