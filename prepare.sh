8a958349b0b13dea2897d06d5349623a0d927622
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-09-30 15:26:03 +0200

    Update the dockerfile with NPM install and create a workflow that launch all the production containers

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./prod-start-all.sh
--- prod-start-all.sh
+++ prod-start-all.sh
@@ -9,10 +9,14 @@ function compile_dir()  # $1 is the dir to get it
     cd ..
 }
 
+echo -e "\033[0;32m ===> LAUNCH BUILD \033[0m"
+compile_dir "module-life-service"
 compile_dir "needs-control-service"
 compile_dir "gateway"
 compile_dir "life-support-service"
-compile_dir "module-life-service"
 compile_dir "resupply-service"
 
+echo -e "\033[0;32m ===> END BUILD \033[0m"
+
+echo -e "\033[0;32m ===> LAUNCH RUN \033[0m"
 docker-compose --project-name soa --file ./docker-compose-prod.yml up -d
