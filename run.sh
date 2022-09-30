8b6cd66b56309cfecaf21491b7752e7e21142c8d
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-09-30 16:26:31 +0200

    MVP du demonstrateur python (sera améliorer et intégrer au DEVOPS plus tard) Pour lancer le test, faire un ./run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+echo -e "\033[0;32m ===> Launch integration.sh \033[0m"
 ./integration.sh
+
+echo -e "\033[0;32m ===> Launch python script \033[0m"
+cd demonstration
+docker build -t demonstration-python .
+docker run -it --rm --name demonstration-python demonstration-python
+cd ..
