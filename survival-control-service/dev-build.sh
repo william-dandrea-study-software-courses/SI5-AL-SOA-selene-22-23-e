a8e457b450d4bcea6530f3ed4a9c5ca12f204e13
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-10-12 22:22:44 +0200

    - Implement the kafka bus in EVA-MISSION - Clean unnecessary for the new docker-compose-dev version

--- survival-control-service/dev-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Building docker image
-echo "[survival-control-service-dev] START : Building image"
-docker build --target docker-development -t "survival-control-service-dev" .
-echo "[survival-control-service-dev] DONE : Building image"
