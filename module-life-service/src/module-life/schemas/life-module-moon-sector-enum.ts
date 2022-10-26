53c508464e8a9adb26d240f301edb3c530e6b262
Laurie-Fernandez <59823387+Laurie-Fernandez@users.noreply.github.com>
2022-10-26 15:38:55 +0200

    Mise en place des secteurs

--- module-life-service/src/module-life/schemas/life-module-moon-sector-enum.ts
+++ module-life-service/src/module-life/schemas/life-module-moon-sector-enum.ts
@@ -4,8 +4,4 @@ export enum AstronautMoonSectorEnumSchema {
     SCIENTIFIC_MODULES = "Modules d'analyse scientifique",
     STRATEGIC_MODULE = 'Module de commandement de la base',
     EMERGENCY_MODULE = 'Module sécurisé',
-    BOARDING_AREA = "Zone d'embarquement",
-    EXPLORATION_1 = "Zone d'exploration 1",
-    EXPLORATION_2 = "Zone d'exploration 2",
-    EXPLORATION_3 = "Zone d'exploration 3",
 }
